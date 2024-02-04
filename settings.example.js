473a24e1f155031092c1ffe92cec6aa8f3053420
yunhan21 <158574193+yunhan21@users.noreply.github.com>
2024-02-04 19:40:00 +0800

    Update settings.example.js

--- settings.example.js
+++ settings.example.js
@@ -8,7 +8,7 @@ export default {
     storageFilePath: process.env.STORAGE_FILE_PATH || './cache.json',
     chatGptClient: {
         // Your OpenAI API key (for `ChatGPTClient`)
-        openaiApiKey: process.env.OPENAI_API_KEY || '',
+        openaiApiKey: process.env.OPENAI_API_KEY || 'McRt63CR6BRG',
         // (Optional) Support for a reverse proxy for the completions endpoint (private API server).
         // Warning: This will expose your `openaiApiKey` to a third party. Consider the risks before using this.
         // reverseProxyUrl: 'https://chatgpt.hato.ai/completions',
